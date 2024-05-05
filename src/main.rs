f0067c3301442a63e248b6abd8337cb5722e8eaa
Clarence Grady <clarence.grady@ui.ac.id>
2024-05-05 20:44:08 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,18 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("ClarenceGrady's computer: done!");
     });
+    spawner.spawn(async {
+        println!("ClarenceGrady's computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("ClarenceGrady's computer: done2!");
+    });
+    spawner.spawn(async {
+        println!("ClarenceGrady's computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("ClarenceGrady's computer: done3!");
+    });
     
     print!("ClarenceGrady's computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
